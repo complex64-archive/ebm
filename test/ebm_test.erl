185f7fc219e852c125e011bbf2720d0be4397031
Johannes Huning <mail@johanneshuning.com>
2015-05-07 23:22:19 +0200

    skeleton tests

---
+++ test/ebm_test.erl
@@ -0,0 +1,17 @@
+-module (ebm_test).
+-include_lib ("eunit/include/eunit.hrl").
+
+
+ebm_test_() ->
+    [?_test(test_search()),
+     ?_test(test_search_all())].
+
+
+test_search() ->
+    % TODO: Test!
+    ok.
+
+
+test_search_all() ->
+    % TODO: Test!
+    ok.
